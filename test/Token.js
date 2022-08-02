372f39043b4257de895f7bb5841d85199dcc62f7
Esther Tan <esthertan@Esthers-MacBook-Pro.local>
2022-08-02 14:45:08 +1000

    Token Transfers

--- test/Token.js
+++ test/Token.js
@@ -1,54 +1,92 @@
-const {expect} = require("chai");
-const {ethers} = require("hardhat");
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
 const tokens = (n) => {
-	return ethers.utils.parseUnits(n.toString(), "ether")
+  return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-describe("Token", () => {
-	let token, accounts, deployer
+describe('Token', () => {
+  let token, accounts, deployer, receiver
 
-	beforeEach(async() => {
-		// Fetch Token from Blockchain
-		const Token = await ethers.getContractFactory("Token")
-		token = await Token.deploy("My Token", "MYTOK", "1000000")
+  beforeEach(async () => {
+    const Token = await ethers.getContractFactory('Token')
+    token = await Token.deploy('Dapp University', 'DAPP', '1000000')
 
-		accounts = await ethers.getSigners()
-		deployer = accounts[0]
-	})
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
+    receiver = accounts[1]
+  })
 
-	describe("Deployment", () => {
-		const name = "My Token"
-		const symbol = "MYTOK"
-		const decimals = "18"
-		const totalSupply = tokens("1000000")
+  describe('Deployment', () => {
+    const name = 'Dapp University'
+    const symbol = 'DAPP'
+    const decimals = '18'
+    const totalSupply = tokens('1000000')
+
+    it('has correct name', async () => {
+      expect(await token.name()).to.equal(name)
+    })
+
+    it('has correct symbol', async () => {
+      expect(await token.symbol()).to.equal(symbol)
+    })
+
+    it('has correct decimals', async () => {
+      expect(await token.decimals()).to.equal(decimals)
+    })
+
+    it('has correct total supply', async () => {
+      expect(await token.totalSupply()).to.equal(totalSupply)
+    })
+
+    it('assigns total supply to deployer', async () => {
+      expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+    })
+
+  })
 
 
-	it("has correct name",async () => {
-		// Read token name
-		expect(await token.name()).to.equal(name)
-	})
+  describe('Sending Tokens', () => {
+    let amount, transaction, result
 
-	it("has correct symbol",async () => {
-		// Read token name
-		// Check that name is correct
-		expect(await token.symbol()).to.equal(symbol)
-	})
+    describe('Success', () => {
 
-	it("has correct decimals",async () => {
-		// Read token name
-		// Check that name is correct
-		expect(await token.decimals()).to.equal(decimals)
-	})
+      beforeEach(async () => {
+        amount = tokens(100)
+        transaction = await token.connect(deployer).transfer(receiver.address, amount)
+        result = await transaction.wait()
+      })
 
-	it("has correct total supply",async () => {
-		// Read token name
-		// Check that name is correct
-		expect(await token.totalSupply()).to.equal(totalSupply)
-	})
+      it('transfers token balances', async () => {
+        expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+        expect(await token.balanceOf(receiver.address)).to.equal(amount)
+      })
 
-	it("assigns total supply to deployer", async () => {
-		expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
-})
-})
+      it('emits a Transfer event', async () => {
+        const event = result.events[0]
+        expect(event.event).to.equal('Transfer')
+
+        const args = event.args
+        expect(args.from).to.equal(deployer.address)
+        expect(args.to).to.equal(receiver.address)
+        expect(args.value).to.equal(amount)
+      })
+
+    })
+
+    describe('Failure', () => {
+      it('rejects insufficient balances', async () => {
+        const invalidAmount = tokens(100000000)
+        await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+      })
+
+      it('rejects invalid recipent', async () => {
+        const amount = tokens(100)
+        await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+      })
+
+    })
+
+  })
+
 })
