9f5a0878df9ab22ee0ee0bd33abea7727dde3af5
Esther Tan <esthertan@Esthers-MacBook-Pro-5.local>
2023-04-26 13:17:24 +1000

    Redux 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,29 @@
 
 import {useEffect} from 'react';
-import {ethers} from 'ethers';
+import {useDispatch} from 'react-redux';
 import config from '../config.json';
-import TOKEN_ABI from '../abis/Token.json';
-import '../App.css';
 
+import {
+  loadProvider, 
+  loadNetwork, 
+  loadAccount,
+  loadToken
+} from '../store/interactions';
 
 function App() {
+  const dispatch = useDispatch()
 
   const loadBlockchainData = async () => {
-    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
-    console.log(accounts[0])
+    await loadAccount(dispatch)
 
     // Connect Ethers to blockchain
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const {chainId} = await provider.getNetwork()
-    console.log(chainId)
+    const provider = loadProvider(dispatch)
+    const chainId = await loadNetwork(provider, dispatch)
+    
 
     // Token Smart Contact
-    const token = new ethers.Contract("0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0", TOKEN_ABI, provider)
-    console.log(token.address)
-    const symbol = await token.symbol()
-    console.log(symbol)
+    await loadToken(provider, config[chainId].dApp.address, dispatch)
+    
 
   }
 
