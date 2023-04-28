8cacf36d88de7b40d27b152812675c1e3aa88bb7
Esther Tan <esthertan@Esthers-MacBook-Pro-5.local>
2023-04-29 00:55:30 +1000

    Redux 2

--- src/components/App.js
+++ src/components/App.js
@@ -7,24 +7,34 @@ import {
   loadProvider, 
   loadNetwork, 
   loadAccount,
-  loadToken
+  loadTokens,
+  loadExchange
+
 } from '../store/interactions';
 
 function App() {
   const dispatch = useDispatch()
 
   const loadBlockchainData = async () => {
-    await loadAccount(dispatch)
+    await loadAccount(provider, dispatch)
 
     // Connect Ethers to blockchain
     const provider = loadProvider(dispatch)
+
+    // Fetch current network's chainId (i.e. hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch)
-    
 
-    // Token Smart Contact
-    await loadToken(provider, config[chainId].dApp.address, dispatch)
-    
+    // Fetch current account & balance from Metamask
+    await loadAccount(provider, dispatch)
 
+    // Load token Smart Contracts
+    const DApp = config[chainId].DApp
+    const mETH = config[chainId].mETH
+    await loadTokens(provider, [DApp.address, mETH.address], dispatch)
+    
+    // Load Exchange Smart Contract
+    const exchangeConfig = config[chainId].exchange
+    await loadExchange(provider, exchangeConfig.address, dispatch)
   }
 
   useEffect(() => {
